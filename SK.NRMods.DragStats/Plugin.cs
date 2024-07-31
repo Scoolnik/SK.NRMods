54049702707b6e1070e1019624a5c45b1b333e22
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2024-07-31 16:34:47 +0500

    Drag stats v1.0.2

--- SK.NRMods.DragStats/Plugin.cs
+++ SK.NRMods.DragStats/Plugin.cs
@@ -5,7 +5,7 @@ using SK.NRMods.DragStats.HarmonyParchers;
 
 namespace SK.NRMods.DragStats
 {
-	[BepInPlugin("SK.NRMods.DragStats", "Drag stats (like 0-100 time)", "1.0.1")]
+	[BepInPlugin("SK.NRMods.DragStats", "Drag stats (like 0-100 time)", "1.0.2")]
 	public class Plugin : BasePlugin
 	{
 		public override void Load()
