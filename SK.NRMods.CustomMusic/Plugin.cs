e3c21e0c2faa51389c6d7905db9ad67f9c0af133
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2024-08-02 23:34:47 +0500

    Custom music v1.0.2

--- SK.NRMods.CustomMusic/Plugin.cs
+++ SK.NRMods.CustomMusic/Plugin.cs
@@ -5,7 +5,7 @@ using SK.NRMods.CustomMusic.HarmonyParchers;
 
 namespace SK.NRMods.CustomMusic
 {
-	[BepInPlugin("SK.NRMods.CustomMusic", "Custom music", "1.0.1")]
+	[BepInPlugin("SK.NRMods.CustomMusic", "Custom music aka NR_LibreMusic", "1.0.2")]
 	public class Plugin : BasePlugin
 	{
 		public override void Load()
