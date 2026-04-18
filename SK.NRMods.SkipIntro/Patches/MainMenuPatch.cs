a9bb0388ad851f1a2d1ac8e7498ff416ed06a49a
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2026-04-18 20:55:19 +0500

    feat: add SkipInto mod

---
+++ SK.NRMods.SkipIntro/Patches/MainMenuPatch.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx.Unity.IL2CPP.Utils;
+using BepInEx.Unity.IL2CPP.Utils.Collections;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SK.NRMods.SkipIntro.Patches
+{
+	internal class MainMenuPatch
+	{
+		private static bool _isLoading = true;
+
+		[HarmonyPatch(typeof(mainMenu), "Start")]
+		[HarmonyPrefix]
+		public static bool BeforeStart()
+		{
+			var god = GodConstant.Instance;
+			if (god.noPlayerData || !god.player_hasCompletedIntro)
+			{
+				_isLoading = false;
+			}
+			return true;
+		}
+
+		[HarmonyPatch(typeof(GodConstant), "loadCurrentDrivingCar")]
+		[HarmonyPrefix]
+		public static bool BeforeSpawnCar()
+		{
+			return !_isLoading;
+		}
+
+		[HarmonyPatch(typeof(mainMenu), "Start")]
+		[HarmonyPostfix]
+		public static void Start(mainMenu __instance)
+		{
+			_isLoading = false;
+			var god = GodConstant.Instance;
+			if (god.noPlayerData || !god.player_hasCompletedIntro)
+			{
+				return;
+			}
+
+			Time.timeScale = 100; // Increase time scale to skip useless pauses caused by "WaitForSeconds"
+			god.StartCoroutine(__instance.goTo_loadingScreen().WrapToManaged());
+		}
+
+		[HarmonyPatch(typeof(GodConstant), "startLoad")]
+		[HarmonyPostfix]
+		public static void ResetTimeScale(mainMenu __instance)
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
---
+++ SK.NRMods.SkipIntro/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using SK.NRMods.SkipIntro.Patches;
+
+namespace SK.NRMods.SkipIntro
+{
+	[BepInPlugin("SK.NRMods.SkipIntro", "Skip that loooooong hello screen", "2.0.0")]
+	public class Plugin : BasePlugin
+	{
+		public override void Load()
+		{
+			Harmony.CreateAndPatchAll(typeof(MainMenuPatch));
+		}
+	}
+}
