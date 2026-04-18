348bde7742427ec67ef392c45eced3c3e98eab6d
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2026-04-19 00:04:27 +0500

    chore(cm): bump version

--- SK.NRMods.CustomMusic/Plugin.cs
+++ SK.NRMods.CustomMusic/Plugin.cs
@@ -5,7 +5,7 @@ using SK.NRMods.CustomMusic.HarmonyPatchers;
 
 namespace SK.NRMods.CustomMusic
 {
-	[BepInPlugin("SK.NRMods.CustomMusic", "Custom music aka NR_LibreMusic", "1.0.2")]
+	[BepInPlugin("SK.NRMods.CustomMusic", "Custom music aka NR_LibreMusic", "2.0.0")]
 	public class Plugin : BasePlugin
 	{
 		public override void Load()
