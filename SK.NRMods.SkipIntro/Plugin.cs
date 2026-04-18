15eba57db518bac17bb998fad8f263b8aa86c2ba
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2026-04-19 00:12:09 +0500

    chore(si): bump version

--- SK.NRMods.SkipIntro/Plugin.cs
+++ SK.NRMods.SkipIntro/Plugin.cs
@@ -5,7 +5,7 @@ using SK.NRMods.SkipIntro.Patches;
 
 namespace SK.NRMods.SkipIntro
 {
-	[BepInPlugin("SK.NRMods.SkipIntro", "Skip that loooooong hello screen", "2.0.0")]
+	[BepInPlugin("SK.NRMods.SkipIntro", "Skip that loooooong hello screen", "2.0.1")]
 	public class Plugin : BasePlugin
 	{
 		public override void Load()
